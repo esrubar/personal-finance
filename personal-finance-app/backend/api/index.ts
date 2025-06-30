10ed582724603082ed9e2f2b532e47b83a5927bf
Estela <estela@MacBook-Pro-de-Estela.local>
2025-06-30 23:51:58 +0200

    New try

--- personal-finance-app/backend/api/index.ts
+++
@@ -1,8 +0,0 @@
-import serverless from 'serverless-http';
-import app from '../src/app';
-
-const handler = serverless(app);
-
-export default async function (req: any, res: any) {
-  return handler(req, res);
-}
--- personal-finance-app/backend/src/app.ts
+++ personal-finance-app/backend/src/app.ts
@@ -1,17 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import { connectDB } from './db';
+import express, { json } from "express";
+import cors from "cors";
+import { config } from "dotenv";
 import expenseRoutes from "./routes/expense.routes";
 import categoryRoutes from "./routes/category.routes";
 import savingProjectRoutes from "./routes/savingProject.routes";
 import userRoutes from "./routes/user.routes";
 import categoryBudgetRoutes from "./routes/categoryBudget.routes";
+import connectDB from "./config/db";
 
-const app = express();
-const PORT = process.env.NEXT_PUBLIC_PORT || 4000;
+config();
+
+export const app = express();
+
+// Conectar a la base de datos
+connectDB();
+
+// Middleware
+app.use(json());
 
 const corsOptions = {
     origin: [
+        "https://personal-finance-backend-csxjr8eya-esrubar-projects.vercel.app",
         "http://localhost:3000",
         "http://localhost:5173",
     ],
@@ -21,7 +30,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
 
 app.get('/', (_req: any, res: any) => res.send('API funcionando 🎉'));
 app.use('/api/expenses', expenseRoutes);
@@ -29,13 +37,5 @@ app.use('/api/categories', categoryRoutes);
 app.use('/api/saving-projects', savingProjectRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/categoryBudgets', categoryBudgetRoutes);
-
-if (process.env.VERCEL !== '1') {
-    connectDB().then(() => {
-        app.listen(PORT, () => {
-            console.log(`🚀 Servidor local en http://localhost:${PORT}`);
-        });
-    });
-}
 
 export default app;
