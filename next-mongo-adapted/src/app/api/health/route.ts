f62a7f456003d05d837c1a8771f4c8e145e7ed7c
Estela <estela@MacBook-Pro-de-Estela.local>
2025-08-19 21:23:49 +0200

    test

--- next-mongo-adapted/src/app/api/health/route.ts
+++ next-mongo-adapted/src/app/api/health/route.ts
@@ -1,11 +1,16 @@
 import {connectDB} from "@config/db";
+import {NextResponse} from "next/server";
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export async function GET() {
-  console.log("Revalidate", process.env.MONGODB_URI, process.env.JWT_SECRET);
+  try {
+  console.log("Revalidate");
   await connectDB();
-  return Response.json({ status: "ok", env: process.env.MONGODB_URI, db: "connected" });
+  return NextResponse.json({ status: "ok", env: process.env.MONGODB_URI, db: "connected" });
+  } catch (e: any) {
+    return NextResponse.json({ error: e.message ?? "Bad Request" }, { status: 400 });
+  }
 }
